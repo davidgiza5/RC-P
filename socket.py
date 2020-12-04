a024bf2a6a52d1d040b423aa5410b0ce1fbf4002
davidgiza5 <72604009+davidgiza5@users.noreply.github.com>
2020-12-04 10:39:27 +0200

    Add files via upload

---
+++ socket.py
@@ -0,0 +1,15 @@
+import socket
+
+class Socket:
+    localIP = "192.168.0.1"
+    localPort = 1234
+    flag  = False
+
+    @staticmethod
+    def init(self, addr='', localPort='1234'):
+        if(Socket.flag == True):
+            exit
+
+        Socket.flag = True
+        self.socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        self.socket.bind((addr,local))
